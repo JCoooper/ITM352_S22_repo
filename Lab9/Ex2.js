d8a987983ebb9a1bbb62e6d7e1aaba4b690cc250
JCoooper <31222322+JCoooper@users.noreply.github.com>
2022-03-10 00:03:09 -1000

    s

--- Lab9/Ex2.js
+++ Lab9/Ex2.js
@@ -1,4 +1,4 @@
-function isNotNegInt(q, returnErrors=false) {
+function isNotNegInt(q, returnErrors=false) { // checks to make sure the string coming in is a non-negative int
   errors = []; // assume no errors at first
   if (Number(q) != q) errors.push("Not a number!"); // Check if string is a number value
   if (q < 0) errors.push("Negative value!"); // Check if it is non-negative
@@ -15,4 +15,34 @@ for(let part of parts_array) {
     console.log(`Part ${part} isNotNegInt ${errs.join(',')}`);
 }
 
+/*
+let show = function() {
+  parts_array.forEach((item,index) => {} ) 
+};
+*/
 
+function checkIt(item, index){
+  console.log(`part ${index} is ${(isNotNegInt(item)?'a':'not a')} quantity`);
+}
+
+
+console.log(parts_array.forEach(checkIt()));
+parts_array.forEeach((item,index) => {console.log(`part ${index} is ${(isNotNegInt(item)?'a':'not a')} quantity`);} ) 
+//console.log(checkIt(parts_array));
+
+
+function download(url, callback) {
+  setTimeout(() => {
+      // script to download the picture here
+      console.log(`Downloading ${url} ...`);
+      picture_data = "image data:XOXOXO";
+  }, 3* 1000);
+  callback(picture_data); 
+}
+
+function process(picture) {
+  console.log(`Processing ${picture}`);
+}
+
+let url = 'https://www.example.comt/big_pic.jpg';
+download(url, process); 
