cf756b1debf94e6d7956b15371ba90d3ebb27adb
JCoooper <31222322+JCoooper@users.noreply.github.com>
2022-03-17 22:58:30 -1000

    s

--- WOD/DayOfTheWeek1/DayOfTheWeek1.js
+++ WOD/DayOfTheWeek1/DayOfTheWeek1.js
@@ -1,72 +1,49 @@
-var day = 14;
-var month = "january";
-var year = 2001;
+var day = 26;
+var month = "march";
+var year = 2013;
 
 var monthKey = {
     january: 0,
-    febuary: 3,
+    february: 3,
     march: 2,
     april: 5,
     may: 0,
     june: 3,
     july: 5,
-    auguest: 1,
+    august: 1,
     september: 4,
     october: 6,
     november: 2,
     december: 4
-  };
+};
 
-  const dayKey = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-
-  var step1 = 0;
-  var step2 = 0;
-  var step3 = 0;
-  var step4 = 0;
-  var step5 = 0;
-  var step6 = 0;
-
+const dayKey =  ['Sunday', 'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday'];
 
 //step 1
-if(month == "january" || month == "febuary") {
+if(month == "January" || month == "Feburary") {
     step1 = year - 1;
 } else {
     step1 = year;
 }
 
-console.log(step1);
-
 //step 2
-step2 = Math.floor(step1 / 4);
-step1 = step1 + (step1 % 4);
+step2 = step1 + Math.floor(step1/4);
 
-console.log(step2 +  " " + step1);
+//step 3
+step3 = step2 - Math.floor(step1/100);
 
-//step3
-step3 = Math.floor(step1 / 100);
-step2 = step2 - (step1 % 100);
-
-console.log(step3 + " " + step2);
-
-//step4
-step4 = Math.floor(step1 / 400);
-step3 = step3 + (step1 % 400);
-
-console.log(step4 + " " + step3);
-
-console.log(step4 + " " + step3);
+//step 4
+step4 = step3 + Math.floor(step1/400);
 
 //step 5
 step5 = step4 + day;
 
-//step6
-console.log(step6)
+//step 6
 step6 = step5 + monthKey[month];
-console.log(step6)
 
 //step 7
-console.log(step6)
 step7 = step6 % 7;
+console.log(step7);
 
-//out 
-console.log(step7 + " " + dayKey[step7]);
+//out
+console.log(`On ${month} ${day} in ${year} it was a ${dayKey[step7]}`);
