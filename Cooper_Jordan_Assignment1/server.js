2720dcb710afd02487b4d1666f131258819ee463
JCoooper <31222322+JCoooper@users.noreply.github.com>
2022-04-01 01:18:51 -1000

    s

--- Cooper_Jordan_Assignment1/server.js
+++ Cooper_Jordan_Assignment1/server.js
@@ -1,7 +1,26 @@
+function isNonNegInt(q, returnerrors = false) {
+   errors = []; // assume no errors at first
+   if (q == '') {
+       q = 0; // handle blank inputs as if they are 0, help from assignment 1 example
+   }
+   if (Number(q) != q) {
+       errors.push("Not a number!"); // Check if string is a number value
+   } else if (q < 0) {
+       errors.push("Negative value!"); // Check if it is non-negative
+   } else if (parseInt(q) != q) {
+       errors.push("Not an integer!"); // Check that it is an integer
+   }
+   return returnerrors ? errors : (errors.length == 0);
+} 
+
+// get items
+var products_array = require(__dirname + '/products.json');
+
 var express = require('express');
 var app = express();
 
-
+//help from stackoverflow
+app.use(express.urlencoded({extended: true}))
 
 // Routing 
 
@@ -12,11 +31,34 @@ app.all('*', function (request, response, next) {
 });
 
 // process purchase request (validate quantities, check quantity available)
-app.get('./purchase', function (request, response, next) {
-   var q = request.body['product_form'];
-   if(typeof q != 'undefined') {
-       response.send(`Thank you for purchasing ${q} things!`);
+app.post("/purchase", function (request, response, next) {
+   //set defaults 
+   var hasValue = false; 
+   var isValidQuantity = true; 
+
+   for (i = 0; i < products_array.length; i++){
+      var inputValue = request.body[`quantity${i}`]; 
+     if (!isNonNegInt(inputValue)){ // if it is not a negative int set isValidQuantity to false
+        isValidQuantity = false; 
+     }
+
+   //Check if a quantity is greater than 0
+   if (inputValue > 0){
+      hasValue = true;
    }
+  
+     if(inputValue > products_array[i].quantity){ 
+        isValidQuantity = false;
+     }
+   }
+
+   // redirect to invoice and store quantities in query string 
+  let params = new URLSearchParams(request.body);
+  if(isValidQuantity == true && hasValue == true) {
+     response.redirect('./invoice.html?'+ params.toString());
+  } else {
+     response.redirect('./index.html?'+ params.toString());
+  }
 });
 
 // route all other GET requests to files in public 
--- Lab13/Ex1b.js
+++ Lab13/Ex1b.js
@@ -1,2 +1,20 @@
-var users_reg_data = require("./user_data.json");
-console.log(users_reg_data);
+var filename = "./user_data.json";
+
+const fs = require("fs");
+if(fs.existsSync(filename)){
+    let stats 
+    console.log
+    var data = fs.readFileSync(filename, 'utf-8');
+    var users = JSON.parse(data);
+    if(typeof users["kazman"] != 'undefined'){
+        console.log(users["kazman"].password);
+    }
+} else {
+    console.log(`${filename} does not exist!`);
+}
+
+
+
+
+//var users_reg_data = require("./user_data.json");
+//console.log(users_reg_data[kazman].password);
