3ed995b1e89a15db23ea91649c8e743b6b55a3d7
JCoooper <31222322+JCoooper@users.noreply.github.com>
2022-05-06 21:23:18 -1000

    s

--- Cooper_Jordan_Assignment3/public/product_data.js
+++ Cooper_Jordan_Assignment3/public/product_data.js
@@ -1,36 +1,142 @@
-let product1 = {
-    name: "Pearl Necklace",
-    price: 150.00,
-    image: "/pics/pearlneck.jpg",
+let diamondproduct1 = {
+    name: "Diamond Watch",
+    price: 30000.00,
+    image: "/pics/diamondwatch.jpg",
     quantity_available: 5
 }
 
-let product2 = {
-    name: "Diamond Ring",
-    price: 3500.00,
-    image: "/pics/diamondring2.jpg",
-    quantity_available: 5
-}
-
-let product3 = {
+let diamondproduct2 = {
     name: "Diamond Necklace",
-    price: 9000.00,
+    price: 6500.00,
     image: "/pics/diamondnecklace.jpg",
     quantity_available: 5
 }
 
-let product4 = {
+let diamondproduct3 = {
+    name: "Diamond Chain Large",
+    price: 10000.00,
+    image: "/pics/diamondchainbig.jpg",
+    quantity_available: 5
+}
+
+let diamondproduct4 = {
+    name: "Diamond Chain Small",
+    price: 6000.00,
+    image: "/pics/diamondchainsmall.jpg",
+    quantity_available: 5
+}
+
+let diamondproduct5 = {
+    name: "Diamond Grill",
+    price: 1500.00,
+    image: "/pics/diamondgrill.jpg",
+    quantity_available: 5
+}
+
+let diamondproduct6 = {
+    name: "Diamond Ring",
+    price: 7000.00,
+    image: "/pics/diamondring2.jpg",
+    quantity_available: 5
+}
+
+let diamondarray = [diamondproduct1, diamondproduct2, diamondproduct3, diamondproduct4, diamondproduct5, diamondproduct6];
+
+//------------------------------gold items--------------------------------------------
+let goldproduct1 = {
     name: "Gold Watch",
-    price: 780.00,
+    price: 10000.00,
     image: "/pics/goldwatch2.jpg",
     quantity_available: 5
 }
 
-let product5 = {
+let goldproduct2 = {
+    name: "Gold Necklace",
+    price: 5000.00,
+    image: "/pics/goldnecklace.jpg",
+    quantity_available: 5
+}
+
+let goldproduct3 = {
+    name: "Gold Chain Large",
+    price: 7000.00,
+    image: "/pics/goldchainbig.jpg",
+    quantity_available: 5
+}
+
+let goldproduct4 = {
+    name: "Gold Chain Small",
+    price: 3000.00,
+    image: "/pics/goldchainsmall.jpg",
+    quantity_available: 5
+}
+
+let goldproduct5 = {
+    name: "Gold Grill",
+    price: 1500.00,
+    image: "/pics/goldgrill.jpg",
+    quantity_available: 5
+}
+
+let goldproduct6 = {
+    name: "Gold Ring",
+    price: 600.00,
+    image: "/pics/goldring.jpg",
+    quantity_available: 5
+}
+
+let goldarray = [goldproduct1, goldproduct2, goldproduct3, goldproduct4, goldproduct5, goldproduct6];
+
+//------------------------------pearl items--------------------------------------------
+let pearlproduct1 = {
+    name: "Pearl Necklace",
+    price: 1000.00,
+    image: "/pics/pearlneck.jpg",
+    quantity_available: 5
+}
+
+let pearlproduct2 = {
     name: "Pearl Bracelet",
-    price: 80.00,
+    price: 700.00,
     image: "/pics/pearlhand.jpg",
     quantity_available: 5
 }
 
-let products_array = [product1, product2, product3, product4, product5];
+let pearlproduct3 = {
+    name: "Pearl Earrings",
+    price: 700.00,
+    image: "/pics/pearlearrings.jpg",
+    quantity_available: 5
+}
+
+let pearlproduct4 = {
+    name: "Pearl Necklace Small",
+    price: 900.00,
+    image: "/pics/pearlnecklacesmall.jpg",
+    quantity_available: 5
+}
+
+let pearlproduct5 = {
+    name: "Pearl Choker",
+    price: 1500.00,
+    image: "/pics/pearlchoker.jpg",
+    quantity_available: 5
+}
+
+let pearlproduct6 = {
+    name: "Loose Pearls",
+    price: 200.00,
+    image: "/pics/loosepearls.jpg",
+    quantity_available: 5
+}
+
+let pearlarray = [pearlproduct1, pearlproduct2, pearlproduct3, pearlproduct4, pearlproduct5, pearlproduct6];
+
+
+
+let products_array = [diamondproduct1, diamondproduct2, diamondproduct3, diamondproduct4, diamondproduct5, diamondproduct6, pearlproduct1, pearlproduct2, pearlproduct3, pearlproduct4, pearlproduct5, pearlproduct6, goldproduct1, goldproduct2, goldproduct3, goldproduct4, goldproduct5, goldproduct6];
+
+
+
+
+//let products_array = [product1, product2, product3, product4, product5];
